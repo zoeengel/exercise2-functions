fe4bf67d7aee42d0a532b6783781681d990e2611
zoeengel <zengel1706@gmail.com>
2020-10-06 11:48:23 +0200

    initial commit

---
+++ exercise2.py
@@ -0,0 +1,13 @@
+import calendar
+def is_leap(year):
+    if year %4 == 0:
+        if year % 100 == 0:
+            if year % 400== 0:
+                return True
+            else:
+                return False
+        return True
+    else:
+        return False
+
+print(is_leap(2001))
